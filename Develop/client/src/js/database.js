7660d833842dffd6c26516647248118db1dd6794
Brain Atoms <badams6453@gmail.com>
2023-06-27 13:16:32 -0400

    corrected database file

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -20,10 +20,10 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('contact', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
    // Use the .add() method on the store and pass in the content.
    const request = store.put({ id: 1, value: content });
